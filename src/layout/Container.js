622f34b073369fa6b7675a9aabe5924628460530
amitroy4 <amitroyrock6071@gmail.com>
2023-05-01 13:09:51 +0600

    container color remove

--- src/layout/Container.js
+++ src/layout/Container.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Container = (props) => {
     return (
-        <div className=' max-w-container mx-auto bg-red-200'>{props.children}</div>
+        <div className=' max-w-container mx-auto'>{props.children}</div>
     )
 }
 
