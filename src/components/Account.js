7b64c79ca00ab440ed93541a45e27eb3373c0dcf
devs-sourav <souravacharjee361@gmail.com>
2023-05-02 14:38:45 +0600

    Account Part Done

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import Container from '../layout/Container';
+
 
 const Account = () => {
+
+  const [accountinfo, setAccountinfo] = useState({});
+  const [load, setLoad] = useState(true);
+
+  useEffect(() => {
+
+    async function account(){
+      const account = await axios.get('https://bwfc-api.vercel.app/account')
+      setAccountinfo(account.data)
+      setLoad(false)
+    }
+    account()
+
+  }, []);
+
+  if(load){
+    return <h1>Loading.....</h1>
+  }
+
+
   return (
-    <div>Account</div>
+    <section className=' pb-3 box-border'>
+      <Container>
+        <div className='flex flex-wrap justify-between font-manrope'>
+          <div>
+            <h4 className='uppercase font-extrabold text-lg text-primary mb-6'>{accountinfo.subTitle}</h4>
+            <h2 className='font-bold text-[50px] leading-[56px] text-black w-[550px] mb-7'>{accountinfo.title}</h2>
+            <p className='w-[515px] text-[18px] leading-[26px] text-[#757095] font-normal'>{accountinfo.paragraph}</p>
+            <a href={accountinfo.button.link} className='inline-block text-center mt-7 w-[162px] rounded-lg bg-primary text-white py-[14px] font-bold text-base leading-[30px] border border-primary hover:bg-white hover:text-primary duration-300 drop-shadow-accbtn'>{accountinfo.button.text}</a>
+          </div>
+          <div className='w-[590px]'>
+            <img className='w-full' src={accountinfo.accountImage}/>
+          </div>
+        </div>
+      </Container>
+    </section>
   )
 }
 
--- src/components/Payment.js
+++ src/components/Payment.js
@@ -23,14 +23,14 @@ const Payment = () => {
 
 
   return (
-    <section className='mt-9 mb-[100px] font-manrope'>
+    <section className='mt-9 mb-[100px] font-manrope box-border'>
       <Container>
         <div className=' relative pt-[75px] pr-28 pl-[70px] pb-[71px] bg-[#F3F7FA] rounded-[20px] flex flex-wrap justify-between'>
           <div className='absolute right-0 -top-[17px]'>
             <img src={payment.paymentShape}/>
           </div>
           <div>
-            <h4 className='font-extrabold text-lg text-primary mb-6'>{payment.subTitle}</h4>
+            <h4 className='font-extrabold uppercase text-lg text-primary mb-6'>{payment.subTitle}</h4>
             <h2 className='font-bold text-[50px] leading-[56px] text-black w-[560px] mb-9'>{payment.title}</h2>
             <p className='w-[515px] text-[18px] leading-[26px] text-[#757095] font-normal'>{payment.paragraph}</p>
           </div>
@@ -38,7 +38,7 @@ const Payment = () => {
             <h4 className='font-bold text-2xl leading-[30px] text-primary mb-6'>Get Started for Free</h4>
             <input className='block w-full pt-[18px] pl-5 pb-3 pr-4 rounded-md text-xl leading-[30px] font-normal text-[#292929] placeholder:text-lg placeholder:font-normal placeholder:text-[#C1BFCB] ' type='text' placeholder='Email Address'/>
             <input className='block w-full pt-[18px] mt-6 mb-6 pl-5 pb-3 pr-4 rounded-md text-xl leading-[30px] font-normal text-[#292929] placeholder:text-lg placeholder:font-normal placeholder:text-[#C1BFCB] ' type='password' placeholder='Password'/>
-            <button className='w-full duration-300 bg-[#FF7F5C] font-bold text-base leading-[30px] border border-[#FF7F5C] uppercase text-white rounded-md pt-4 pb-[14px] hover:bg-white hover:border-primary hover:text-primary hover:border'>Get Started</button>
+            <a href='#' className='inline-block text-center w-full duration-300 bg-[#FF7F5C] font-bold text-base leading-[30px] border  border-[#FF7F5C] uppercase text-white rounded-md pt-4 pb-[14px] hover:bg-white hover:border-primary hover:text-primary hover:border drop-shadow-paybtn'>Get Started</a>
           </div>
         </div>
       </Container>
