0b8e229e928342edb894e6b6b5c648dabe066e16
devs-sourav <souravacharjee361@gmail.com>
2023-05-04 21:13:16 +0600

    all Problem solve

--- src/components/Account.js
+++ src/components/Account.js
@@ -27,12 +27,12 @@ const Account = () => {
   return (
     <section className=' pb-3 box-border'>
       <Container>
-        <div className='flex flex-wrap justify-between font-manrope'>
+        <div className='flex justify-between font-manrope'>
           <div>
             <h4 className='uppercase font-extrabold text-lg text-primary mb-6'>{accountinfo.subTitle}</h4>
             <h2 className='font-bold text-[50px] leading-[56px] text-black w-[550px] mb-7'>{accountinfo.title}</h2>
-            <p className='w-[515px] text-[18px] leading-[26px] text-[#757095] font-normal'>{accountinfo.paragraph}</p>
-            <a href={accountinfo.button.link} className='inline-block text-center mt-7 w-[162px] rounded-lg bg-primary text-white py-[14px] font-bold text-base leading-[30px] border border-primary hover:bg-white hover:text-primary duration-300 drop-shadow-accbtn'>{accountinfo.button.text}</a>
+            <p className='w-[515px] mb-7 text-[18px] leading-[26px] text-[#757095] font-normal'>{accountinfo.paragraph}</p>
+            <a href={accountinfo.button.link} className='inline-block text-center w-[162px] rounded-lg bg-primary text-white py-[14px] font-bold text-base leading-[30px] border border-primary hover:bg-white hover:text-primary duration-300 drop-shadow-accbtn'>{accountinfo.button.text}</a>
           </div>
           <div className='w-[590px]'>
             <img className='w-full' src={accountinfo.accountImage} />
--- src/components/Choose.js
+++ src/components/Choose.js
@@ -18,20 +18,21 @@ const Choose = () => {
   if (load) {
     return <h1>Loading...</h1>
   }
-
+  
 
   return (
     <section className='font-manrope'>
       <Container>
-        <div className='w-[1172px] bg-secondary pt-[56px] rounded-[20px] pl-[81px] pr-[81px] pb-[58px] relative'>
+        <div className='w-full bg-secondary pt-[56px] rounded-[20px] pl-[81px] pr-[81px] pb-[58px] relative'>
           <h4 className='uppercase font-extrabold text-[18px] text-primary text-center'>{choose.subTitle}</h4>
           <h1 className='font-bold text-[50px] text-center pt-[14px]'>{choose.title}</h1>
-          <div className='flex justify-between flex flex-wrap mt-[79px] gap-y-[40px]'>
+          <div className='flex justify-between flex-wrap mt-[79px] gap-y-[40px]'>
             {choose.items.map((citems) => (
               citems.visibility && <div className='w-[48%] bg-white pt-[59px] pb-[37px] pl-[120px] pr-[47px] rounded-[20px] relative'>
                 <h3 className='font-extrabold text-[24px]'>{citems.title}</h3>
-                <p className='font-regular text-[18px] pt-[22px]'>{citems.details}</p>
-                <div className={`h-[58px] w-[58px] rounded-full bg-[${citems.circleColor}] absolute top-[59px] left-[30px]`}></div>
+                <p className='font-normal text-[18px] pt-[22px]'>{citems.details}</p>
+
+                <div style={{backgroundColor: true ?  citems.circleColor: ' '}} className={`h-[58px] w-[58px] rounded-full  absolute top-[59px] left-[30px]`} ></div>
               </div>
 
             ))}
